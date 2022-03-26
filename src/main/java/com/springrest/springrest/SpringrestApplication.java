d43549c6d12cd030fa9b780ab8e66b6f93955862
imasr <ashishs723@gmail.com>
2022-03-26 18:13:28 +0530

    Post user make public

--- src/main/java/com/springrest/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/springrest/SpringrestApplication.java
@@ -2,12 +2,19 @@ package com.springrest.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bcryptPassword() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
