6698c369dcfb8a2c0a0175f0d84698917234be7f
imasr <ashishs723@gmail.com>
2022-07-08 19:00:23 +0530

    deleted unused

--- src/main/java/com/springrest/springrest/controller/Home.java
+++ src/main/java/com/springrest/springrest/controller/Home.java
@@ -1,7 +1,4 @@
 package com.springrest.springrest.controller;
-
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
